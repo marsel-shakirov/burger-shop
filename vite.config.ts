4ca470cf56ca14ed72eef33ea4906b4d94a147e1
Shakirov Marsel <imanager666@gmail.com>
2026-07-25 15:08:12 +0300

    first commit

---
+++ vite.config.ts
@@ -0,0 +1,19 @@
+import babel from '@rolldown/plugin-babel'
+import react, { reactCompilerPreset } from '@vitejs/plugin-react'
+import { fileURLToPath, URL } from 'node:url'
+import { defineConfig } from 'vite'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [
+    react(),
+    babel({ presets: [reactCompilerPreset()] })
+  ],
+
+  resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url)),
+      },
+    },
+
+})
