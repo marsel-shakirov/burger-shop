46d98d1a3e92718728d09a6d958b4947b5e642bf
Shakirov Marsel <imanager666@gmail.com>
2026-08-10 22:41:45 +0300

    header-done

---
+++ ui/CartSummary.tsx
@@ -0,0 +1,16 @@
+import { CartIcon } from '@/shared/ui/icon';
+
+import type { CartSummaryProps } from '../model/cart.types';
+
+export const CartSummary = ({ price, quantity }: CartSummaryProps) => {
+  return (
+    <>
+      <span className="">{price}&nbsp;₽</span>
+      <span className="w-px bg-white/25"></span>
+      <span className="flex gap-x-2">
+        <CartIcon className="h-5 w-5" />
+        {quantity}
+      </span>
+    </>
+  );
+};
