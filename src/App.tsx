315dc4c866871f8438100cd78e40a00a38f28e8d
Shakirov Marsel <imanager666@gmail.com>
2026-08-10 23:17:58 +0300

    setting up the linter

--- src/App.tsx
+++
@@ -1,7 +0,0 @@
-export const App = () => {
-  return (
-    <div className="flex items-center justify-center bg-sky-700">
-      <h1 className="text-2xl font-bold text-[#2C2C2C]">Hello world!</h1>
-    </div>
-  );
-};
