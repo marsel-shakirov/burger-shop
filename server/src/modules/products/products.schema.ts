aec004343fd5a81c5c2c85da0f3fdbc7674f20a4
Shakirov Marsel <imanager666@gmail.com>
2026-09-09 22:29:09 +0300

    fix(products-schema): api/products returns an error BadRequest if the category is entered incorrectly

--- server/src/modules/products/products.schema.ts
+++ server/src/modules/products/products.schema.ts
@@ -1,7 +1,7 @@
 import { z } from 'zod';
 
 export const getProductsQuerySchema = z.object({
-  category: z.string().optional(),
+  category: z.enum(['beef', 'chicken', 'fish']).optional(),
   sort: z.enum(['popularity', 'price', 'rating']).default('popularity'),
   order: z.enum(['asc', 'desc']).default('asc'),
 });
